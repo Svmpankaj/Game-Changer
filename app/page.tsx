9c11e510cfda42e3ca4fc924c291a2f10bf1fd55
Svmpankaj <svmpankaj@gmail.com>
2023-07-01 23:53:43 +0530

    fetch games results

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
+type Game = {
+  id: number
+  background_image: string
+  rating: number
+  number: string
+}
+
 const getGames = async () => {
   const res = await fetch(`https://api.rawg.io/api/games?key=${process.env.RAWG}`)
   if (!res.ok) {
     throw new Error("failed to fetch")
   }
   const data = await res.json()
-  return data
+  return data.results
 }
 
 export default async function Home() {
