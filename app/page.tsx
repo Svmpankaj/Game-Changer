666d15ab677a9e617977229fd843b4b433b29a89
Svmpankaj <svmpankaj@gmail.com>
2023-07-02 15:29:45 +0530

    games load with image is completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,43 @@
-type Game = {
-  id: number
-  background_image: string
-  rating: number
-  number: string
-}
+import Image from "next/image";
 
-const getGames = async () => {
-  const res = await fetch(`https://api.rawg.io/api/games?key=${process.env.RAWG}`)
+type Game = {
+  id: number;
+  background_image: string;
+  rating: number;
+  name: string;
+};
+
+const getGames = async (): Promise<Game[]> => {
+  const res = await fetch(
+    `https://api.rawg.io/api/games?key=${process.env.RAWG}`
+  );
   if (!res.ok) {
-    throw new Error("failed to fetch")
+    throw new Error("failed to fetch");
   }
-  const data = await res.json()
-  return data.results
-}
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  const data = await res.json();
+  return data.results;
+};
 
 export default async function Home() {
-  const games = await getGames()
-  console.log(games)
+  const games = await getGames();
+
   return (
-    <main>
-      <h1>hello</h1>
+    <main className="m-24 rounded-md grid grid-cols-4 gap-12">
+      {games.map((game) => (
+        <div className="col-span-4 md:col-span-2 lg:col-span-1" key={game.id}>
+          <h1>{game.name}</h1>
+          <p className="font-bold text-sm mb-4">Rating: {game.rating}</p>
+          <div className="aspect-video relative">
+            <Image
+              src={game.background_image}
+              fill
+              className="object-cover rounded-md"
+              alt={game.name}
+            />
+          </div>
+        </div>
+      ))}
     </main>
-  )
+  );
 }
